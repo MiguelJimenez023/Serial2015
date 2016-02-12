274d72eb6e30b4053ccc43d1d1b3569af80da3cc
MiguelJimenez023 <migueeel11.7@gmail.com>
2016-02-12 19:28:07 +0100

    serial1.4

---
+++ serial1.4/serial1.4.ino/serial1.4.ino.ino
@@ -0,0 +1,34 @@
+/*************************************************************************************************
+**                                                                                              **
+**                              Descripció del programa                                         **
+**                                                                                              **
+*************************************************************************************************/
+
+//******  Includes  ******************************************************************************
+
+
+//******  Variables   ****************************************************************************
+
+
+//******  Setup  *********************************************************************************
+void setup()              
+{
+  Serial.begin(9600); 
+Serial.println("Escull el numero de l'operacio qe vols realitzar?");  // prints
+  
+  
+  Serial.println("1. Comprovar numero de tarjeta de credit");  // prints
+  
+  
+  Serial.println("2. Comprovar numero de compte bancari");  // prints
+  
+  
+  Serial.println("3. Buscar un digit perdut de tarjeta de credit");  // prints  
+}
+
+void loop()                     
+{
+  
+  
+}
+
